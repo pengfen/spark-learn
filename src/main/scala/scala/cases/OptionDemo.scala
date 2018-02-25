adfa8e1c8b8c7387fde921d40c0d42f7a9ffe379
pengfen <caopeng8787@163.com>
2018-02-25 18:24:09 +0800

    f

--- src/main/scala/scala/cases/OptionDemo.scala
+++ src/main/scala/scala/cases/OptionDemo.scala
@@ -1,6 +1,11 @@
 package scala.cases
 
-
+/**
+  * 在scala中option类型样例类用来表示可能存在　也可能不存在的值
+  * Option的子类有Some和None
+  * Some包装了某个值
+  * None表示没有值
+  */
 object OptionDemo {
   def main(args: Array[String]) {
     val map = Map("a" -> 1, "b" -> 2)
@@ -10,6 +15,7 @@ object OptionDemo {
       case None => 0
     }
     println(v)
+
     //更好的方式
     val v1 = map.getOrElse("c", 0)
     println(v1)
--- src/main/scala/scala/cases/PartialFuncDemo.scala
+++ src/main/scala/scala/cases/PartialFuncDemo.scala
@@ -1,7 +1,10 @@
 package scala.cases
 
 /**
-  * Created by ZX on 2016/4/5.
+  * 被包在花括号内没有match的一组case语句是一个偏函数 它是PartialFunction[A, B］的一个实例
+  * A代表参数类型
+  * B代表返回类型　常用作输入模式匹配
+  *
   */
 object PartialFuncDemo  {
 
