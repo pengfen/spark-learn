efdc37b3bc97a6456e4ea42d9b15efacc5724381
pengfen <caopeng8787@163.com>
2018-03-07 21:37:35 +0800

    f

--- src/main/scala/spark/pro_user/move_bs_user.sh
+++ src/main/scala/spark/pro_user/move_bs_user.sh
@@ -4,7 +4,7 @@
 date_now=`date +%Y%m%d`
 
 # 1. 先备份数据 bs_user.log.bak_20180304
-cp -a /home/ricky/data/spark/basic/bs_user.log /home/ricky/data/spark/basic/bs_user.log.bak_${date_now}
+cp -a /home/ricky/data/spark/basic/bs_user_${date_now}.log /home/ricky/data/spark/basic/bs_user._${date_now}.log.bak
 
 # 2. 移到数据至data/pro/user
-mv /home/ricky/data/spark/basic/bs_user.log /home/ricky/data/pro/user
+mv /home/ricky/data/spark/basic/bs_user_${date_now}.log /home/ricky/data/pro/user
